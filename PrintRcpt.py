4ce6acd26221510d76e4c9db1474a79f77e7d587
Odxen <91438798+Odxen@users.noreply.github.com>
2021-10-13 22:39:58 +0000

    Update PrintRcpt.py

--- PrintRcpt.py
+++ PrintRcpt.py
@@ -1,36 +1,30 @@
 import random
 
-#ask the clinet if they want a receipt for their transaction or not
-
-loginDetails =["Anne", "10011000"]  #Dummy login details
-amtWithdrawn =["GHS200"] #dummy amount withdrawn
-currentBalance =["GHS400"] #dummy current balance
-
-def rcptPrint():
+def rcptPrint(withdrawn):
+    # CHECK IF USER WOULD LIKE TO PRINT A RECEIPT FOR THE TRANSACTION
     while True:
-        optn = input("Do you want a receipt for your transaction?(y/n):")
-        if optn == "y":
-            print(".........Transaction Receipt............\n")
-            print("****************************************\n")
-            print("Transaction is now complete.\n")                       
-            print("Transaction number: "+str(random.randint(10000,100000000))+"\n")
-            print("Account details: "+str(loginDetails[0])+"\n") 
-            print("Amount withdrawn: "+str(amtWithdrawn[0])+"\n")      
-            print("Available balance: "+str(currentBalance[0])+"\n")                 
-            print("Thank you for choosing Tech Seals Bank\n")
-            print("*************************************")
+        optn = input("Do you want a receipt for your transaction? (Y/N):  ").upper()
+        
+        # CHECK IF THE USER SAYS YES AND PRINT A RECEIPT FOR THE TRANSACTION i.e DISPLAY SPECIFICS OF THE TRANSACTION
+        if optn == "Y":
+            print(("\n \n.........Transaction Receipt............\n").upper())
+            print("******************************************\n \n")
+            print(("Transaction is now complete.\n").upper())                       
+            print("Transaction number: "+str(random.randint(100000000000,999999999999))+"\n")
+            print("Account Name: "+str(session['username']).upper()+"\n") 
+            print("Amount Withdrawn: "+str(withdrawn[0])+" "+str(withdrawn[1])+"\n")      
+            print("Available Balance: "+str(withdrawn[2])+"\n \n")                 
+            print("*******************************************\n \n")
+            print("Thank you for choosing Tech Seals Bank\n \n")
+            print("*******************************************\n \n")
             break
-
-        elif optn == "n":
-            print("""
-                -----------------------------------------
-                | Thank you for choosing Tech Seals Bank |
-                |         Visit us again!                |
-                -----------------------------------------
-            """)
+        
+        # CHECK IF THE USER SAYS NO STOP THE LOOP
+        elif optn == "N":
             break   
-        else:
-            print("Wrong command!  Enter 'y' for yes and 'n' for NO.\n")
-            continue
 
-rcptPrint()
+        # CHECK IF THE USER INPUT IS INCORRECT AND ASK FOR THE CORRECT INPUT
+        else:
+            print("Wrong command!  Enter 'Y' for Yes and 'N' for No.\n")
+            continue
+    return
